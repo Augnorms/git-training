34924ecccedfeb886f051b11e7db2340a4af1cd6
Augustine Normanyo <augustine.normanyo@amalitech.com>
2024-10-12 12:39:01 +0000

    added a new line of code to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 //feat1 added this
-const jenam = "my name is jenam"
-const ivan = "Ivan is my second name"
+const jenam = "my name is jenam";
+const ivan = "Ivan is my second name";
+const name1 = "Ivan jenam is my son";
